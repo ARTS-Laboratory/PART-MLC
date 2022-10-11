2e1f1ce384199dace20403162626ce0e9217e0bb
Zhymir Thompson <dzhymir@gmail.com>
2022-10-11 19:33:06 -0400

    forecaster rework

--- time_series_forecasting/Online_RNN/src/custom_models/forecaster_model.py
+++ time_series_forecasting/Online_RNN/src/custom_models/forecaster_model.py
@@ -5,7 +5,7 @@ import torch
 from multiprocessing import Process, Lock, Pipe, Queue
 import numpy as np
 
-from constants import PIPE_SENTINEL
+from constants import PIPE_SENTINEL, PIPE_CONFIRMATION
 
 # Types
 from multiprocessing.queues import Queue as QueueType
@@ -57,22 +57,23 @@ class Forecaster:
 #     end = time_skip + (seq_len << 1)  # inclusive end index
 #     return start, end
 
-
+# TODO fix data processing, time_skip is not clear enough
 # Function implementation
-def run_forecaster(pair: Forecaster, data, seq_len, time_skip: int, use_torch=True, **kwargs):
+def run_forecaster(pair: Forecaster, data, input_size, time_skip: int, use_torch=True, **kwargs):
     """
     # TODO pair will be changed to be two models in this function. predictor and learner
     :param pair:
     :param data: Data to send to predictor and learner
     :type data: np.ndarray or torch.Tensor
     :type data: torch.Tensor or np.ndarray
-    :param int seq_len: Number of elements model looks at at any one time
+    :param int input_size: Number of elements model looks at at any one time
     :param int time_skip: Number of elements to skip between current and past prediction
     :param bool use_torch: Whether the model to use is a torch model
     """
     # Regular variables
+    seq_length = 10
     start = 0
-    end = time_skip + (seq_len << 1)  # inclusive end index
+    end = time_skip + (input_size << 1)  # inclusive end index
     if use_torch and not isinstance(data, torch.Tensor):
         data = torch.as_tensor(data)  # Might need to add if clause if not ndarray
     # TODO make typing run better here
@@ -82,7 +83,7 @@ def run_forecaster(pair: Forecaster, data, seq_len, time_skip: int, use_torch=Tr
         end_index = data.size(1) - end
     else:
         raise NotImplementedError
-    print(end_index)
+    # print(end_index)
     # TODO function call needs parameters predictor and learner instead of Forecaster
     # Other Process Variables
     loss_fn = torch.nn.MSELoss()
@@ -91,6 +92,7 @@ def run_forecaster(pair: Forecaster, data, seq_len, time_skip: int, use_torch=Tr
     curr_q: QueueType = Queue()
     curr_out: QueueType = Queue()
     hist_q: QueueType = Queue()
+    hist_out: QueueType = Queue()
     update_lock: LockType = Lock()
     parent_param, child_param = Pipe()
     # Start processes
@@ -99,7 +101,7 @@ def run_forecaster(pair: Forecaster, data, seq_len, time_skip: int, use_torch=Tr
         args=(pair.predictor, curr_q, curr_out, parent_param))
     q: Process = Process(
         target=make_improvements_torch_pipe,
-        args=(pair.learner, hist_q, loss_fn, optimizer, child_param))
+        args=(pair.learner, hist_q, loss_fn, 10, optimizer, child_param))
     try:
         p.start()
         q.start()
@@ -108,16 +110,16 @@ def run_forecaster(pair: Forecaster, data, seq_len, time_skip: int, use_torch=Tr
         print(f'Iterating over array of length {len(data)} from 0 to {end_index}')
         if isinstance(data, torch.Tensor):
             for idx in range(start, end_index):
-                end = idx + time_skip + (seq_len << 1)  # inclusive end index
-                curr_q.put(data[:, (idx + seq_len + time_skip): end].detach())
-                hist_q.put([data[:, idx: idx + seq_len].detach(), data[:, end].unsqueeze(1).detach()])
-                time.sleep(0.125)  # TODO this is a workaround to handle when learner finishes first, please fix
+                end = idx + time_skip + (input_size << 1)  # inclusive end index
+                curr_q.put(data[:, (idx + input_size + time_skip): end].detach())
+                hist_q.put([data[:, idx: idx + input_size].detach(), data[:, end].unsqueeze(1).detach()])
+                time.sleep(0.075)  # TODO this is a workaround to handle when learner finishes first, please fix
                 # Add sleep here for time delay?
         elif isinstance(data, np.ndarray):
             for idx in range(start, end_index):
-                end = idx + time_skip + (seq_len << 1)  # inclusive end index
-                curr_q.put(data[:, (idx + seq_len + time_skip): end])
-                hist_q.put([data[:, idx: idx + seq_len], data[:, end]])
+                end = idx + time_skip + (input_size << 1)  # inclusive end index
+                curr_q.put(data[:, (idx + input_size + time_skip): end])
+                hist_q.put([data[:, idx: idx + input_size], data[:, end]])
                 # time.sleep(5)
                 # Add sleep here for time delay?
         else:
@@ -138,9 +140,6 @@ def run_forecaster(pair: Forecaster, data, seq_len, time_skip: int, use_torch=Tr
             del item
             item = curr_out.get()
         print(f'Number of results {len(results)}')
-        # TODO Change this to be parameter, it is very confusing and potentially unstable
-        # np.savetxt('./results/output.txt', results)
-        # np.savetxt('./results/output.csv', results, delimiter=',')
         # Close processes
         p.join()
         print(f'Process predictor exit code: {p.exitcode}')
@@ -158,9 +157,8 @@ def run_forecaster(pair: Forecaster, data, seq_len, time_skip: int, use_torch=Tr
         parent_param.close()
         child_param.close()
         del curr_q, curr_out, hist_q, parent_param, child_param
-    del update_lock
-
-    return results
+        del update_lock
+    return results  # list of tensors, fix
 
 
 # TODO make special Queue class to iterate until None
@@ -195,15 +193,16 @@ def make_predictions(model, queue_or_data: QueueType, output_queue: QueueType = 
         data = queue_or_data.get(timeout=20)  # Change this later
         if isinstance(model, RecurrentNeuralNetworkTorch):
             data = torch.as_tensor(data)
+        hidden = model.make_hidden_state()  # Add batch size if batched
         while data is not None:  # TODO this check will only break when get times out
             if lock is not None:
                 with lock:
-                    prediction, hidden = model.predict(data)
+                    prediction, hidden = model.predict(data, hidden)
                 # lock.acquire()
                 # prediction = model.predict(data)
                 # lock.release()
             else:
-                prediction, hidden = model.predict(data)
+                prediction, hidden = model.predict(data, hidden)
             if isinstance(prediction, torch.Tensor):
                 prediction = prediction.detach()
             output_queue.put(prediction)
@@ -215,8 +214,8 @@ def make_predictions(model, queue_or_data: QueueType, output_queue: QueueType = 
         output_queue.put(None)
     except Empty:
         raise TimeoutError('Queue timed out waiting for data.')
-    except Exception:
-        raise ChildProcessError('Something unexpected went wrong in predictor.')
+    # except Exception:
+    #     raise ChildProcessError('Something unexpected went wrong in predictor.')
     finally:
         output_queue.close()
     # No way to send data back without this, assume no return
@@ -224,7 +223,9 @@ def make_predictions(model, queue_or_data: QueueType, output_queue: QueueType = 
     #     return data
 
 
-def make_predictions_torch_pipe(model, input_queue: QueueType, output_queue: QueueType, pipe: PipeType):
+def make_predictions_torch_pipe(
+        model, input_queue: QueueType, output_queue: QueueType,
+        pipe: PipeType):
     """Make predictions on input data, update weights with data from pipe, and send predictions through queue.
 
         Model makes predictions on input data from input_queue. It updates
@@ -239,14 +240,16 @@ def make_predictions_torch_pipe(model, input_queue: QueueType, output_queue: Que
         :param PipeType pipe: Pipe connection to receive model updates through
         """
     count = 0
+    # TODO make hidden only if RNN type
     try:
         data = input_queue.get(timeout=20)  # Change this later
+        hidden = model.make_hidden_state()  # Add batch size if batched
         while data is not None:
             count += 1
             print(f'Predictor looking at data slice #{count}', flush=True)
             if isinstance(model, RecurrentNeuralNetworkTorch):
                 data = torch.as_tensor(data)
-            prediction, hidden = model.predict(data)
+            prediction, hidden = model.predict(data, hidden)
             if isinstance(model, RecurrentNeuralNetworkTorch):
                 prediction = prediction.detach()
             output_queue.put(prediction)
@@ -255,16 +258,20 @@ def make_predictions_torch_pipe(model, input_queue: QueueType, output_queue: Que
                 new_state = pipe.recv()
                 torch_update_state(model, new_state)
                 del new_state
-            data = input_queue.get(timeout=20)  # Change this later
+                pipe.send(PIPE_CONFIRMATION)
+            data = input_queue.get(timeout=5)  # Change this later
         del data
         output_queue.put(None)
     except Empty:
         raise TimeoutError('Queue timed out waiting for data.')
     except BrokenPipeError:
         raise ChildProcessError('A pipe error occurred.')
-    except Exception:
-        raise ChildProcessError('Something unexpected went wrong in predictor.')
+    # except Exception:
+    #     raise ChildProcessError('Something unexpected went wrong in predictor.')
+    # else:
+    #     pipe.send(PIPE_SENTINEL)
     finally:
+        pipe.send(PIPE_SENTINEL)
         output_queue.close()
         pipe.close()
         print('Predictor pipe closed.', flush=True)
@@ -292,54 +299,129 @@ def make_predictions_torch_pipe(model, input_queue: QueueType, output_queue: Que
 
 
 def make_improvements_torch_pipe(
-        learner: torch.nn.Module, queue: QueueType, loss_fn,
+        learner: torch.nn.Module, in_queue: QueueType, loss_fn, seq_length,
         optimizer: torch.optim.Optimizer, out_pipe: PipeType):
     """ Update weights of learner and send to optimizer via pipe.
 
+    :param seq_length:
     :param learner: Model for learning from predictions
     :type learner: torch.Module
-    :param queue: Multiprocessing queue to receive input data
-    :type queue: QueueType
+    :param in_queue: Multiprocessing queue to receive input data
+    :type in_queue: QueueType
     :param loss_fn: Function to compute loss between learner predictions and real values
     :param torch.optim.Optimizer optimizer: Optimizer to update learner
     :param PipeType out_pipe: Connection to send updates to predictor
     :rtype: None
     """
     # TODO Returning the loss in another queue would be nice
-    # TODO Detect when other process is closed and stop sending data
-    # TODO Continue to update optimizer until predictor gets next from model
+    predictor_done, predictor_updated = False, True
     count = 0
     # This may be worse than a giant try/catch block, will look into
     try:
-        (data, actual) = queue.get(timeout=20)  # Change this later
-        while data is not None:
+        (data, actual) = in_queue.get(timeout=20)  # Change this later
+        hidden = learner.make_hidden_state()
+        while data is not None and not predictor_done:
             count += 1
             print(f'Learner looking at data slice #{count}', flush=True)
             if isinstance(learner, RecurrentNeuralNetworkTorch):
                 data = torch.as_tensor(data)
-            prediction, hidden = learner.predict(data)
-            loss = loss_fn(prediction, actual)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            print('Sending data.', flush=True)
-            out_pipe.send(learner.state_dict())
-            print('Data sent.', flush=True)
+            prediction, hidden = learner.predict(data, hidden)
+            if count % seq_length == 0:  # if enough forward passes have passed
+                loss = loss_fn(prediction, actual)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                hidden = learner.make_hidden_state()  # Add batch size if batched
+            if out_pipe.poll(timeout=0.05):
+                out_data = out_pipe.recv()
+                if out_data == PIPE_CONFIRMATION:  # if confirmed, send next
+                    print('Confirmation received, sending next', flush=True)
+                    out_pipe.send(learner.state_dict())
+                    print('Learner sent data', flush=True)
+                elif out_data == PIPE_SENTINEL:  # if sentinel, end
+                    print('Sentinel received, ending', flush=True)
+                    predictor_done = True
+                else:
+                    raise NotImplementedError('Unexpected pipe message.')
+
+            # if not out_pipe.poll(timeout=0.05):  # If nothing to receive
+            #     print('Sending data.', flush=True)
+            #     out_pipe.send(learner.state_dict())
+            #     print('Data sent.', flush=True)
+            # else:
+            #     print('Data received.', flush=True)
+            #     out_data = out_pipe.recv()
+            #     if out_data is PIPE_SENTINEL:
+            #         print('Pipe sentinel detected.', flush=True)
+            #         predictor_done = True
+            #         break
             del data, actual
-            (data, actual) = queue.get(timeout=20)
+            (data, actual) = in_queue.get(timeout=5)
         del data, actual
         print('out of while loop', flush=True)
     except TimeoutError:
         raise ChildProcessError('Process timed out waiting for data.')
     except BrokenPipeError:
+        print('Broken pipe error', flush=True)
         raise ChildProcessError('A pipe error occurred.')
-    except Exception:
-        raise ChildProcessError('Something unexpected went wrong in learner.')
+    # except Exception:
+    #     raise ChildProcessError('Something unexpected went wrong in learner.')
     finally:
         print('Learner pipe closing.', flush=True)
         out_pipe.close()
         print('Learner pipe closed.', flush=True)
 
+
+def make_improvements_torch_queue(
+        learner: torch.nn.Module, in_queue: QueueType, loss_fn, seq_length,
+        optimizer: torch.optim.Optimizer, update_queue: QueueType,
+        is_rnn: bool = True, out_pred_queue=None, out_loss_queue=None, timeout=None):
+    """ Update weights of learner and send to optimizer via pipe.
+    # TODO update docstring for new signature
+    :param seq_length:
+    :param learner: Model for learning from predictions
+    :type learner: torch.Module
+    :param in_queue: Multiprocessing queue to receive input data
+    :type in_queue: QueueType
+    :param loss_fn: Function to compute loss between learner predictions and real values
+    :param torch.optim.Optimizer optimizer: Optimizer to update learner
+    :param PipeType update_queue: Connection to send updates to predictor
+    :rtype: None
+    """
+    # TODO Returning the loss in another queue would be nice
+    # TODO Detect when other process is closed and stop sending data
+    # TODO Continue to update optimizer until predictor gets next from model
+    predictor_done = False
+    count = 0
+    # This may be worse than a giant try/catch block, will look into
+    try:
+        (data, actual) = in_queue.get(timeout=timeout)
+        hidden = learner.make_hidden_state()
+        while data is not None and not predictor_done:
+            count += 1
+            print(f'Learner looking at data slice #{count}', flush=True)
+            if isinstance(learner, RecurrentNeuralNetworkTorch):
+                data = torch.as_tensor(data)
+            prediction, hidden = learner.predict(data, hidden)
+            if count % seq_length == 0:  # if enough forward passes have passed
+                loss = loss_fn(prediction, actual)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                hidden = learner.make_hidden_state()  # Add batch size if batched
+            out_data = update_queue.get()  # TODO change to block and wait
+            if out_data is PIPE_SENTINEL:
+                predictor_done = True
+                break
+            else:
+                update_queue.put(learner.state_dict())  # TODO change to block and wait
+            del data, actual
+            (data, actual) = in_queue.get(timeout=timeout)
+        del data, actual
+    except TimeoutError:
+        raise ChildProcessError('Process timed out waiting for data.')
+    finally:
+        update_queue.close()
 
 
 # def make_improvements_torch_sm(
@@ -393,30 +475,3 @@ def update_weights_lock(model: torch.nn.Module, new_weights, lock: LockType):
         """
     with lock:
         update_weights(model, new_weights)
-
-
-def say_hi(queue, out):
-    name = queue.get(timeout=5)
-    time.sleep(3)
-    while name is not None:
-        print(f'hi {name}', flush=True)
-        name = queue.get(timeout=5)
-        time.sleep(3)
-        out.put(name)
-    out_queue.close()
-
-
-if __name__ == '__main__':
-    q = Queue()
-    out_queue = Queue()
-    p = Process(target=say_hi, args=(q, out_queue))
-    q.put('bob')
-    q.put('charlie')
-    q.put('deaundre')
-    q.put('ellen')
-    p.start()
-    q.close()
-    print('queue closed')
-    name = out_queue.get()
-    print(name)
-    p.join()
